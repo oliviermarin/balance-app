4f47c4e94e8027ed3994c2b091ce0390513a61be
oliviermarin <oliviermarin92@gmail.com>
2017-08-14 19:09:36 +0200

    Suppression fichier

--- src/main/resources/data-h2.sql
+++
@@ -1,5 +0,0 @@
-insert into attendees(name, email) values('Pierre', 'pierre.dupont@gmail.com');
-insert into attendees(name, email) values('Paul', 'paul.dupont@gmail.com');
-insert into attendees(name, email) values('Jacques', 'jacques.dupont@gmail.com');
-
-insert into tri_counts(title, currency, description, attendee_id) values('Opal Festival', 'EUR', 'Festival de musique en Normandie', 1);
