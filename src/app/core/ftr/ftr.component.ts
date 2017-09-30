cc6091630033c9feadb01e9f2970b5fdc61b6d9d
oliviermarin <oliviermarin92@gmail.com>
2017-09-30 10:44:59 +0200

    footer global de l'application

---
+++ src/app/core/ftr/ftr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ftr',
+  templateUrl: './ftr.component.html',
+  styleUrls: ['./ftr.component.scss']
+})
+export class FtrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
